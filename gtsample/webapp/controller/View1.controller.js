19290fd6a34f98d1d1df64a85d081bc7bee0bc66
Yash Vijay <yash.vijay@ge.com>
2024-02-05 10:24:07 +0000

    merge testing

--- gtsample/webapp/controller/View1.controller.js
+++ gtsample/webapp/controller/View1.controller.js
@@ -9,6 +9,7 @@ sap.ui.define([
 
         return Controller.extend("gtsample.controller.View1", {
             onInit: function () {
+                console.log("initial Load");
                 sap.m.MessageToast.show("Welcome to Fiori")
             }
         });
