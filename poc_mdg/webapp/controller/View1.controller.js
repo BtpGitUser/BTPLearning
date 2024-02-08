85cf27f8b9e14ffbe24aee89845d5836d0de069e
Yash Vijay <yash.vijay@ge.com>
2024-02-08 07:16:46 +0000

    [Feture2][POC][BTP][initial]

---
+++ poc_mdg/webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("pocmdg.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ poc_mdg/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"poc_mdg/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
